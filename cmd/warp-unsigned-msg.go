b24e571fcfb862c6a7bfa7cf49d08a68cb69a3a8
shaokun <605416017@qq.com>
2023-11-16 13:41:23 +0800

    fixed warp message event

--- cmd/warp-unsigned-msg.go
+++ cmd/warp-unsigned-msg.go
@@ -18,19 +18,13 @@ var warpUnsignedMsgCmd = &cobra.Command{
 	Use:   "warp-unsigned-msg",
 	Short: "warp the send payload to unsigned message",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 4 {
-			return errors.New("the four params should be: payload sourceChainID networkID sourceAddress")
+		if len(args) != 6 {
+			return errors.New("the four params should be: payload sourceChainID networkID sourceAddress destinationChainID destinationAddress")
 		}
-		//0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000040000000000000000000000008db97c7cece249c2b98bdc0226cc4c2a57bf52fc00000000000000000000000069b5c70fabf28b48ec130984cd439efb11ca634700000000000000000000000000000000000000000000000000000000000186a000000000000000000000000000000000000000000000000000000000000000e000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000120000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-		decode, err := hexutil.Decode(args[0])
+		payload, err := hexutil.Decode(args[0])
 		if err != nil {
 			return err
 		}
-		input, err := warp.UnpackSendWarpMessageInput(decode)
-		if err != nil {
-			return err
-		}
-		//sourceChainID, err := ids.FromString("DFcwVrEKk46moY2iLTBA6r72HtWEMPf8sr81zXGK5Mno66AKo")
 		sourceChainID, err := ids.FromString(args[1])
 		if err != nil {
 			return err
@@ -40,13 +34,10 @@ var warpUnsignedMsgCmd = &cobra.Command{
 			return err
 		}
 		var (
-			destinationChainID = input.DestinationChainID
-			//sourceAddress      = common.HexToAddress("0xAb72527b0F669B18f8322437b4eB98C2DaD92bf7")
 			sourceAddress      = common.HexToAddress(args[3])
-			destinationAddress = input.DestinationAddress
-			payload            = input.Payload
+			destinationChainID = common.HexToHash(args[4])
+			destinationAddress = common.HexToAddress(args[5])
 		)
-
 		addressedPayload, err := paylaod2.NewAddressedPayload(
 			sourceAddress,
 			destinationChainID,
